9b5de171a2c3a16a189bedbfb5d018996bab149c
leonardocunhads <82096432+leonardocunhads@users.noreply.github.com>
2023-04-20 15:18:24 +0100

    final

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+// import CSS
+import "./Thanks.css";
+// import Icons
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
 
-const Thanks = () => {
+const emojiData = {
+  unsatisfied: <BsFillEmojiFrownFill />,
+  neutral: <BsFillEmojiNeutralFill />,
+  satisfied: <BsFillEmojiSmileFill />,
+  very_satisfied: <BsFillEmojiHeartEyesFill />,
+};
+
+const Thanks = ({ data }) => {
   return (
-    <div>
-      <h2>Thanks</h2>
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>
+        A sua opinião é muito importante, em breve você receberá um cupom de 10%
+        para sua próxima compra.
+      </p>
+      <p>Para completar sua avaliação clique no botão enviar, abaixo.</p>
+      <h3>Aqui está o resumo da sua avaliação, {data.name}:</h3>
+      <p className="review-data">
+        <span>Satisfação na compra do produto: </span>
+        {emojiData[data.review]}
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+        {data.comment}
+      </p>
     </div>
   );
 };
